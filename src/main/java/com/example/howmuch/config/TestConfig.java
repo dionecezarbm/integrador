5ee2b4b7dcd6cf6d4ed4db4515fba3b4006576ae
Dione Martins <dione.martins@sankhya.com.br>
2020-02-12 21:11:12 -0200

    Cadastro de produtos

--- src/main/java/com/example/howmuch/config/TestConfig.java
+++ src/main/java/com/example/howmuch/config/TestConfig.java
@@ -20,9 +20,9 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Product p4 = new Product(null, "PC Gamer", "Produto sem imagem", 2500.0, "", "123456");
-		Product p1 = new Product(null, "Açaí Frooty 2L", "Açaí Natural Frooty Pote 2 Litros", 38.5, "https://www.paodeacucar.com/img/uploads/1/154/615154.png", "1039326");
-		Product p2 = new Product(null, "Picanha aturatta 900g", "Picanha Resfriada Maturatta FRIBOI Peça 900g", 71.0, "https://www.paodeacucar.com/img/uploads/1/777/518777.jpg", "0049009");
-		Product p3 = new Product(null, "Heineken 350ml", "Cerveja Heineken Lata 350ml", 3.49, "", "0790642");
+		Product p1 = new Product(null, "Negresco", "Biscoito Recheado Negresco 140g", 1.5, "https://nagumonew.vteximg.com.br/arquivos/ids/191554-1000-1000/BISCOITO-TRADICIONAL-NEGRESCO-NESTLE-140G-866255_1.jpg", "7891000023808");
+		Product p2 = new Product(null, "Club Social Crostini", "Club Social Crostini - Sabor original 20g", 1.0, "https://s3-sa-east-1.amazonaws.com/fz-imagens/wp-content/uploads/2018/04/12121916/Biscoito-Salgado-Sabor-Original-Crostini-Club-Social.jpeg", "7622210782281");
+		Product p3 = new Product(null, "Desodorante Todo Dia", "Desodorante Natura Todo Dia 80g", 17.0, "https://static.carrefour.com.br/medias/sys_master/images/images/h7a/h4d/h00/h00/13157432426526.jpg", "7899846061442");
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
 	}
